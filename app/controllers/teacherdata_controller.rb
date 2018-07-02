6af3c887f11c94dd2e6da1b29d5ebd8a8d206a1d
shubham <shindeshubham861@gmail.com>
2018-07-02 16:28:54 +0530

    take it

--- app/controllers/teacherdata_controller.rb
+++ app/controllers/teacherdata_controller.rb
@@ -3,7 +3,9 @@ class TeacherdataController < ApplicationController
 
   	 @day = params[:day]
     @teacher = params[:teacher]
+    @std=Timetable.where(teacher: @teacher,day: @day).ids.to_s.split("[")[1].split("]")[2].nil?
   	@standards=Timetable.where(teacher: @teacher,day: @day).ids
+
 
   end
 end
